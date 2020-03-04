736bb1be305391ee7e7c69ad7b4e0928186e1059
David Ghazi <davidaghazi@gmail.com>
2020-03-03 19:21:15 -0500

    added homepage files

---
+++ homepage.js
@@ -0,0 +1,39 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$('.homePage').append('<div class="welcome">Welcome Page</div>')
+
+$('.home').on('click',function(){
+    console.log('home clicked')
+});
+
+$('.artist').on('click',function(){
+    console.log('artist clicked')
+});
+
+$('.genre').on('click',function(){
+    console.log('genre clicked')
+});
+
+$('.song').on('click',function(){
+    console.log('song clicked')
+});
+
+$('.playlist').on('click',function(){
+    console.log('playlist clicked')
+});
